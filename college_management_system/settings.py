877ad3c886fdbc968c922bf7b4fa25d272140315
yuldashevasadbek <asadbekliverpool@mail.ru>
2023-05-04 13:30:27 +0500

    112

--- college_management_system/settings.py
+++ college_management_system/settings.py
@@ -28,8 +28,8 @@ SECRET_KEY = 'f2zx8*lb*em*-*b+!&1lpp&$_9q9kmkar+l3x90do@s(+sr&x7'  # Consider us
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-# ALLOWED_HOSTS = ['smswithdjango.herokuapp.com']
-ALLOWED_HOSTS = ['127.0.0.1']  # Not recommended but useful in dev mode
+
+ALLOWED_HOSTS = ['.pythonanywhere.com','127.0.0.1']  # Not recommended but useful in dev mode
 
 
 # Application definition
